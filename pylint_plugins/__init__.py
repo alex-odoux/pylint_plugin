1744d57eac18b3054817e275c0bb81276336c105
Alex Odoux <alex@odoux.net>
2022-12-02 17:46:14 +0100

    sc analysis through github action
    attempt to add pylint issues
    
    tabs (#2)

--- pylint_plugins/__init__.py
+++ pylint_plugins/__init__.py
@@ -1,13 +1,16 @@
 from typing import TYPE_CHECKING
 
+from pylint_plugins.sonar_issues_reporter import SonarIssuesReporter
+
 from pylint_plugins.deprecated_classes import DeprecatedClassesChecker
 from pylint_plugins.deprecated_modules import DeprecatedModulesChecker
 
 
-if TYPE_CHECKING:
-    from pylint.lint import PyLinter
+from pylint.lint import PyLinter
 
 
-def register(linter: "PyLinter") -> None:
+def register(linter: PyLinter) -> None:
+    linter.register_reporter(SonarIssuesReporter)
+
     linter.register_checker(DeprecatedClassesChecker(linter))
     linter.register_checker(DeprecatedModulesChecker(linter))
